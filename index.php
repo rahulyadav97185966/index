f38c7ef7d79e5eabd5b87c67defeac39721d44d2
OperatorSDK Katacoda <operator-sdk@example.com>
2020-03-10 14:10:47 +0000

    hello

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<html>
-<head></head>
-<body>
-<h1>This is Heading H1 </h1>
-
-<h1>This is Heading H1 </h1>
-<h1>this is another</h1>
-<h2>hello sagar</h2>
-</body>
-</html>
+echo "hello sagar yadav";
